8c6547466225a35468a97d094956cb8ead8fe328
john-works <workjohn265@gmail.com>
2025-10-09 17:17:36 +0300

    New changes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+
+
+//moverments
+Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
+Route::get('/authors/indexs', [AuthorController::class, 'indexs'])->name('authors.indexs');
+Route::get('/authors/create', [AuthorController::class, 'create'])->name('authors.create');
+Route::post('/authors', [AuthorController::class, 'store'])->name('authors.store');
+Route::get('/authors/{author}', [AuthorController::class, 'show'])->name('authors.show');
+Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
+Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
+Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
