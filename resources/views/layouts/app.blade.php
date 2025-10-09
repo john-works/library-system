835bf94caad50b1df4301bc98d4415a1a074fdc5
john-works <workjohn265@gmail.com>
2025-10-09 18:24:44 +0300

    New changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -164,13 +164,13 @@
 
 
     </div>
-    <script src="assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{ asset('assets/js/bootstrap.bundle.min.js')}}"></script>
 
-    <script src="assets/vendors/apexcharts/apexcharts.js"></script>
-    <script src="assets/js/pages/dashboard.js"></script>
+    <script src="{{ asset('assets/vendors/apexcharts/apexcharts.js')}}"></script>
+    <script src="{{ asset('assets/js/pages/dashboard.js')}}"></script>
 
-    <script src="assets/js/main.js"></script>
+    <script src="{{ asset('assets/js/main.js')}}"></script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\BorrowerController;
 
 Route::get('/', function () {
     return redirect()->route('login');
@@ -17,6 +19,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
+
 //moverments
 Route::get('/authors', [AuthorController::class, 'index'])->name('authors.index');
 Route::get('/authors/indexs', [AuthorController::class, 'indexs'])->name('authors.indexs');
@@ -26,3 +29,28 @@ Route::get('/authors/{author}', [AuthorController::class, 'show'])->name('author
 Route::get('/authors/{author}/edit', [AuthorController::class, 'edit'])->name('authors.edit');
 Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('authors.update');
 Route::delete('/authors/{author}', [AuthorController::class, 'destroy'])->name('authors.destroy');
+
+
+
+
+
+//books
+Route::get('/books', [AuthorController::class, 'index'])->name('books.index');
+Route::get('/books/indexs', [AuthorController::class, 'indexs'])->name('books.indexs');
+Route::get('/books/create', [AuthorController::class, 'create'])->name('books.create');
+Route::post('/books', [AuthorController::class, 'store'])->name('books.store');
+Route::get('/books/{book}', [AuthorController::class, 'show'])->name('books.show');
+Route::get('/books/{book}/edit', [AuthorController::class, 'edit'])->name('books.edit');
+Route::put('/books/{book}', [AuthorController::class, 'update'])->name('books.update');
+Route::delete('/books/{book}', [AuthorController::class, 'destroy'])->name('books.destroy');
+
+
+//books
+Route::get('/boborrowersoks', [AuthorController::class, 'index'])->name('borrowers.index');
+Route::get('/borrowers/indexs', [AuthorController::class, 'indexs'])->name('borrowers.indexs');
+Route::get('/borrowers/create', [AuthorController::class, 'create'])->name('borrowers.create');
+Route::post('/borrowers', [AuthorController::class, 'store'])->name('borrowers.store');
+Route::get('/borrowers/{borrower}', [AuthorController::class, 'show'])->name('borrowers.show');
+Route::get('/borrowers/{borrower}/edit', [AuthorController::class, 'edit'])->name('borrowers.edit');
+Route::put('/borrowers/{borrower}', [AuthorController::class, 'update'])->name('borrowers.update');
+Route::delete('/borrowers/{borrower}', [AuthorController::class, 'destroy'])->name('borrowers.destroy');
